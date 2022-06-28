38a6b1d60d53d88a004ef5321a5e8ea0aa11ff43
simonenholm <simonenholm@hotmail.com>
2022-06-28 11:39:50 +0200

    Created ProductRepository

--- src/main/java/com/example/ShoppingCart/ProductRepository.java
+++ src/main/java/com/example/ShoppingCart/ProductRepository.java
@@ -2,6 +2,9 @@ package com.example.ShoppingCart;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Long, Product> {
 
+    Product findById(Long id);
 }
