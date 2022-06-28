a7798755f6414f2cc730f0eb51a6a7bfebb43056
VanjaJonasson <vanja_j@hotmail.com>
2022-06-28 11:42:51 +0200

    Shop Controller updated.

--- src/main/java/com/example/ShoppingCart/ShopController.java
+++ src/main/java/com/example/ShoppingCart/ShopController.java
@@ -2,25 +2,33 @@ package com.example.ShoppingCart;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 public class ShopController {
 
+
     @Autowired
     private ProductRepository repository;
 
     @GetMapping("/products")
-    public List<Product> products() {
-        return (List<Product>) repository.findAll();
+    public String products(Model model) {
+        List<Product> products = repository.findAll();
+        model.addAttribute("products", products);
+        return "products";
     }
 
     @GetMapping("/product/{id}")
-    public Product product(@PathVariable Long id) {
-        return repository.findById(id).get();
+    public String product(Model model, @PathVariable Long id) {
+        Product product = repository.findById(id).get();
+        model.addAttribute("product", product);
+        return "product";
     }
+
 
 }
